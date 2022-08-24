26d14a6efac96b51b6a059bc6d4883692629f105
Bogdan Bedyukh <bogdan.bedyukh@sondeos.com.ar>
2022-08-24 00:32:34 -0300

    Se realiza desafío Nº3 - Arrays

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const totalDistance = parseInt(prompt('Ingrese la distancia total de la ruta en km: '))
-if (totalDistance <= 0) {
-  console.log('La distancia debe ser mayor a 0.')
-}
-const fuelConsumption = parseInt(prompt('Ingrese la cantidad de combustible que se consume cada 100km: '))
-if (fuelConsumption <= 0) {
-  console.log('La cantidad de combustible debe ser mayor a 0.')
+let history = []
+let isRunning = true
+
+try {
+  while (isRunning) {
+    const fullName = prompt('Ingresá tu nombre completo.')
+    assertValueOrFail(fullName)
+
+    const totalDistance = parseInt(prompt('Ingresá la distancia total de la ruta en km: '))
+    assertValueOrFail(totalDistance)
+    if (totalDistance <= 0) {
+      console.log('La distancia debe ser mayor a 0.')
+    }
+
+    const fuelConsumption = parseInt(prompt('Ingresá la cantidad de combustible que se consume cada 100km: '))
+    assertValueOrFail(fuelConsumption)
+    if (fuelConsumption <= 0) {
+      console.log('La cantidad de combustible debe ser mayor a 0.')
+    }
+
+    const price = parseInt(prompt('Ingresá el precio del combustible: '))
+    assertValueOrFail(price)
+    if (price <= 0) {
+      console.log('El precio debe ser mayor a 0.')
+    }
+    const totalCost = (totalDistance / 100 * fuelConsumption * price).toFixed(2)
+    const totalFuel = (totalDistance / 100 * fuelConsumption).toFixed(2)
+
+    history = [...history, fullName]
+    alert(`El costo total de la ruta es de $${totalCost} y se necesita ${totalFuel} litros de combustible.`)
+
+    if (!isRunning) {
+      console.log(`Se agregó a <${fullName}> al historial de cálculos.`)
+    }
+  }
+} catch (err) {
+  console.log(err)
 }
 
-const price = parseInt(prompt('Ingrese el precio del combustible: '))
-if (price <= 0) {
-  console.log('El precio debe ser mayor a 0.')
-}
-const totalCost = (totalDistance / 100 * fuelConsumption * price).toFixed(2)
-const totalFuel = (totalDistance / 100 * fuelConsumption).toFixed(2)
-
-if (!totalDistance || !fuelConsumption || !price) {
-  alert('No se pudo calcular el costo total.')
-} else {
-  alert(`El costo total de la ruta es de $${totalCost} y se necesita ${totalFuel} litros de combustible.`)
+function assertValueOrFail (value) {
+  if (!value) {
+    isRunning = false
+    console.log(`Personas que realizaron el cálculo <${history.length}>`)
+    console.log(history)
+    throw new Error('Se canceló el proceso del cálculo.')
+  }
 }
